59f1a504b08825aa89ade9bf1f81a8e9558e2cd0
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-04-11 15:33:56 -0600

    db env controllers

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,15 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
+
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+//dashboard route?
+
+
+//added
+router.use((req, res) => {
+    res.status(404).end();
+})
 
 module.exports = router;
